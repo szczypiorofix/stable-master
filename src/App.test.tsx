d20f8b04e90ae5f34554454b29d3fabcdab4f42b
szczypiorofix <szczypiorofix@o2.pl>
2025-02-18 20:58:59 +0100

    Add Vitest

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import { screen, render } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import App from './App';
+
+describe('App', () => {
+    it('renders main App component', () => {
+        render(<App />);
+        expect(screen.getByText("Vite + React")).toBeInTheDocument();
+    });
+});
