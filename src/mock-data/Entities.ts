766374791c84db505ac06a7344c63781dccee9b0
szczypiorofix <szczypiorofix@o2.pl>
2025-02-23 08:49:30 +0100

    Move mocked data to entities

--- src/mock-data/Entities.ts
+++ src/mock-data/Entities.ts
@@ -1,4 +1,5 @@
-import { Horse, Person } from '../@types';
+import { Horse, Person, Stable } from '../@types';
+import { RoleEnum } from '../shared/enums/Roles.enum.ts';
 
 export const people: Person[] = [
     {
@@ -9,6 +10,7 @@ export const people: Person[] = [
         address: '123 Main St',
         sex: 'M',
         isRider: true,
+        role: RoleEnum.RIDER,
     },
 ];
 
@@ -54,5 +56,31 @@ export const horses: Horse[] = [
         gallery: [],
         sex: 'gelding',
         onPaddock: true,
+    },
+];
+
+export const stables: Stable[] = [
+    {
+        name: 'Dream Valley Stables',
+        address: '123 Main St',
+        phone: '123-456-7890',
+        email: 'stable@email',
+        website: 'https://stable.com',
+        logo: '/src/assets/images/stable1.jpg',
+        description: 'This is a stable. It is a very nice stable.',
+        facilities: ['arena', 'round pen', 'wash rack'],
+        services: ['boarding', 'training', 'lessons'],
+        socialMedia: {
+            facebook: 'https://facebook.com/stable',
+            instagram: 'https://instagram.com/stable',
+            twitter: 'https://twitter.com/stable',
+        },
+        location: {
+            latitude: 0,
+            longitude: 0,
+        },
+        gallery: [],
+        team: people,
+        horses: horses,
     },
 ];
