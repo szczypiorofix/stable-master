3447c01f33872a4d163ad38105c6db264fd2800a
szczypiorofix <szczypiorofix@o2.pl>
2025-03-30 11:08:51 +0200

    Change entry file for nest application. Add API module

---
+++ backend/src/app.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { ApiModule } from './api/api.module';
+
+async function bootstrap() {
+    const app = await NestFactory.create(ApiModule);
+    await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
