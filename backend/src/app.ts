d4f99ff2a88644f2de9b7f3254e2b172b4ea6d4e
szczypiorofix <szczypiorofix@o2.pl>
2025-03-31 13:14:27 +0200

    Add docker files

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { ApiModule } from './api/api.module';
 import { API_VERSION } from './shared/constants';
 
+const PORT: string | number = process.env.PORT ?? 3000;
+
 async function bootstrap() {
     const app = await NestFactory.create(ApiModule);
     app.setGlobalPrefix(API_VERSION);
     app.enableCors();
-    await app.listen(process.env.PORT ?? 3000);
+    await app.listen(PORT);
 }
 bootstrap()
-    .then(() => console.log('Frontend application started'))
+    .then(() => console.log('Nest application started at port ' + PORT))
     .catch((err) => console.log(err));
