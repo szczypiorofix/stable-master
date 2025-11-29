372f12f25d5e073b16eba321ab9882a98d1ba97a
szczypiorofix <szczypiorofix@o2.pl>
2025-11-29 08:17:54 +0100

    Add missing staff component to index

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -2,4 +2,5 @@ export * from './home/Home.tsx';
 export * from './horses/Horses.tsx';
 export * from './schedules/Schedules.tsx';
 export * from './settings/Settings.tsx';
+export * from './staff/Staff.tsx';
 export * from './stalls/Stalls.tsx';
