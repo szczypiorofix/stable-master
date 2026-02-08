697c3cb48b1971d73e131d216c8477b808a54f7c
szczypiorofix <szczypiorofix@o2.pl>
2026-02-08 21:45:30 +0100

    Move settings tabs to separate folders

--- frontend/src/pages/settings/SettingsTabs.tsx
+++ frontend/src/pages/settings/SettingsTabs.tsx
@@ -2,8 +2,8 @@ import { CustomTabs } from '../../components/tabs/CustomTabs.tsx';
 import { CustomTabItem } from '../../components/tabs/CustomTabs.types.ts';
 
 import { DefinesSettingsTab } from './tabs/defines/DefinesSettingsTab.tsx';
-import { MainSettingsTab } from './tabs/MainSettingsTab.tsx';
-import { OtherSettingsTab } from './tabs/OtherSettingsTab.tsx';
+import { MainSettingsTab } from './tabs/main/MainSettingsTab.tsx';
+import { OtherSettingsTab } from './tabs/other/OtherSettingsTab.tsx';
 
 export function SettingsTabs() {
     const items: Array<CustomTabItem> = [
